ccff1d0e003c815838d1a6bbc114f6714afb38f0
Chirag Pandit <chirag1000p@gmail.com>
2026-03-29 14:46:28 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 5000;
 const express = require("express");
 const cors = require("cors");
 const sqlite3 = require("sqlite3").verbose();
@@ -198,6 +199,6 @@ app.post("/orders", (req, res) => {
 });
 
 // ================= SERVER =================
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
